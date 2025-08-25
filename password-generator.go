190b34885dc35e51103be7712f0b81c7521f45cc
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2025-08-25 16:55:36 -0300

    Add files via upload

---
+++ password-generator.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+const (
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    specialBytes = "!@#$%^&*()_+-=[]{}\\|;':\",.<>/?`~"
+    numBytes = "0123456789"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func generatePassword(length int, useLetters bool, useSpecial bool, useNum bool) string {
+    b := make([]byte, length)
+    for i := range b {
+        if useLetters {
+            b[i] = letterBytes[rand.Intn(len(letterBytes))]
+        } else if useSpecial {
+            b[i] = specialBytes[rand.Intn(len(specialBytes))]
+        } else if useNum {
+            b[i] = numBytes[rand.Intn(len(numBytes))]
+        }
+    }
+    return string(b)
+}
+
+
+	func main() {
+		password := generatePassword(12, true, true, true)
+		fmt.Println("random password:", password)
+	}
